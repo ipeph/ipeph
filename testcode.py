c31dbe0064a928e9178813066790c9a1bb40d2d2
ipeph <viperthehero@gmail.com>
2023-07-31 12:02:13 +0800

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -15,6 +15,8 @@ from netmiko import (
 import os
 import shutil
 from operator import itemgetter
+import csv
+import threading
 
 def network_connection_ip(*input):
     return {
@@ -63,17 +65,59 @@ def check_devices (inventory, devtype, username, password, session):
                 new_output = convert_input(output,"input_rate")
                 new_output = sorted(output, key=itemgetter('input_rate'), reverse=True)
 
+                message = ""
                 total = len(new_output)
                 # total_up = 0
                 # print("Total Interface Is : ",str(len(output)))
-                for i in range(0,total):
+                for i in range(0,9):
                     print("Interface Name : {} || Status : {} || Input Rate : {} || Output Rate : {} ".format(new_output[i]['interface'],new_output[i]['link_status'],int(new_output[i]['input_rate']),new_output[i]['output_rate']))
+                    message +=  "Interface Name : {} || Status : {} || Input Rate : {} || Output Rate : {} \n".format(new_output[i]['interface'],new_output[i]['link_status'],int(new_output[i]['input_rate']),new_output[i]['output_rate'])
                 #     if (output[i]['status'] == "up"):
                 #         print(output[i])
                 #         total_up += 1
                 # print("Total Interface Up " + str(total_up))
 
-                # write_file(hostname,str(output))
+                write_file(hostname,message)
+                # print(output)
+        except (NetmikoAuthenticationException, NetmikoTimeoutException, ReadTimeout) as error:
+            print(str(error))
+            # write_file(hostname,str(error))
+
+def check_devices2(inventory, devtype, username, password, session):
+    for line in inventory:
+        hostname = line['hostname']
+        network_devices = network_connection_ip(
+            devtype,
+            hostname,
+            username,
+            password,
+            True,
+            session + line['hostname']
+        )
+        print("Checking Devices .... " + hostname)
+        try:
+            with ConnectHandler(**network_devices) as net_connect:
+                output = net_connect.send_command(
+                    "show interfaces | i line|rate | ex fifo",
+                    read_timeout=120,
+                    use_textfsm=True
+                )
+                new_output = convert_input(output,"input_rate")
+                new_output = sorted(output, key=itemgetter('input_rate'), reverse=True)
+
+                message = ""
+                total = len(new_output)
+                # total_up = 0
+                # print("Total Interface Is : ",str(len(output)))
+                for i in range(0,9):
+                    print("Interface Name : {} || Status : {} || Input Rate : {} || Output Rate : {} ".format(new_output[i]['interface'],new_output[i]['link_status'],int(new_output[i]['input_rate']),new_output[i]['output_rate']))
+                    message =+  "Interface Name : {} || Status : {} || Input Rate : {} || Output Rate : {} ".format(new_output[i]['interface'],new_output[i]['link_status'],int(new_output[i]['input_rate']),new_output[i]['output_rate'])
+                #     if (output[i]['status'] == "up"):
+                #         print(output[i])
+                #         total_up += 1
+                # print("Total Interface Up " + str(total_up))
+
+                write_file(hostname,message)
                 # print(output)
         except (NetmikoAuthenticationException, NetmikoTimeoutException, ReadTimeout) as error:
             print(str(error))
@@ -95,7 +139,29 @@ def main():
     print("##########")
     print("Start Checking Network Devices")
     print("##########")
-    check_devices("/home/svc_netlog_id/code/inventory", "cisco_ios", "SVC_NW_BCK_AUTO", "qqNfoM1chMxS8BE", "/home/svc_netlog_id/code/")
+    # check_devices("/home/svc_netlog_id/code/inventory", "cisco_ios", "SVC_NW_BCK_AUTO", "qqNfoM1chMxS8BE", "/home/svc_netlog_id/code/")
+    device_list = []
+    file = open("/home/svc_netlog_id/code/inventory.csv","r")
+    readcsv = csv.DictReader(file)
+    for row in readcsv:
+        device_dict = {
+            "ip": row['ipaddress'],
+            "hostname" : row ['hostname']
+        }
+        device_list.append(device_dict)
 
+    # print(device_list)
+    # check_devices2(device_list, "cisco_ios", "SVC_NW_BCK_AUTO", "qqNfoM1chMxS8BE", "/home/svc_netlog_id/code/")
+
+    process_checkdevices1 = threading.Thread(target=check_devices, args=("/home/svc_netlog_id/code/inventory", "cisco_ios", "SVC_NW_BCK_AUTO", "qqNfoM1chMxS8BE", "/home/svc_netlog_id/code/",))
+    process_checkdevices1.start()
+
+    process_checkdevices2 = threading.Thread(target=check_devices, args=("/home/svc_netlog_id/code/inventory2", "cisco_ios", "SVC_NW_BCK_AUTO", "qqNfoM1chMxS8BE", "/home/svc_netlog_id/code/",))
+    process_checkdevices2.start()
+
+    process_checkdevices1.join()
+    process_checkdevices2.join()
+    
+    
 if __name__ == "__main__":
     main()
